f31b6a57daac7f3e8624bd5ddb963f51cdc25456
Pritesh <bhuravanepritesh@gmail.com>
2025-05-18 17:45:43 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Button, Container, Card } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CustomNavbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Container className="mt-5">
-      <Card>
-        <Card.Header>
-          <h1>DevOps Mini Project</h1>
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>CI/CD Pipeline Demo</Card.Title>
-          <Card.Text>
-            This is a simple React + Bootstrap application for demonstrating a CI/CD pipeline using GitHub, Jenkins, and Docker.
-          </Card.Text>
-          <Button variant="primary">Click Me</Button>
-        </Card.Body>
-      </Card>
-    </Container>
+    <Router>
+      <CustomNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
